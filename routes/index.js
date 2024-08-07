779467981841b57c8e898201513b37e86774aca0
DevBasem <engr.basemahmed@gmail.com>
2024-08-07 12:14:25 -0700

    task 3

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
